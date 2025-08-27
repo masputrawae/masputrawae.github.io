2ab5de7cfdce5ebaabef603af547da96f089f454
Masputrawae <masputrawae.official@gmail.com>
2025-08-28 06:39:53 +0700

    Menambahkan slugfy.js untuk templater

---
+++ templates/scripts/slugfy.js
@@ -0,0 +1,9 @@
+module.exports = function (text) {
+  return text
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, "_")
+    .replace(/[^a-z0-9_]/g, "_")
+    .replace(/_+/g, "_")
+    .replace(/^_|_$/g, "");
+};
