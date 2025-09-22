edc24518b7087b6dff955df640d6845557ffe2c1
Masputrawae <masputrawae.official@gmail.com>
2025-09-23 00:07:30 +0700

    Changed On 📅 : 2025-09-23 00:07:30

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-import { linkPreviewHandler } from "./link-preview";
-import { searchHandler } from "./search";
-
-const main = () => {
-  searchHandler();
-  linkPreviewHandler();
-};
-
-document.addEventListener("DOMContentLoaded", main);
