d32d12422d42a44b79627b6fcabe04a3f15bbabb
masputrawae <masputrawae@users.noreply.github.com>
2026-04-06 09:53:14 +0700

    menambah fungsi pengubah tema

--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -1,7 +1,9 @@
 import { menuHandler } from './menu'
+import { themeHandler } from './theme'
 
 function main() {
   document.addEventListener('astro:page-load', () => {
+    themeHandler()
     menuHandler()
   })
 }
---
+++ src/assets/ts/theme.ts
@@ -0,0 +1,20 @@
+interface ThemeSwitchType {
+  html: HTMLElement
+  mq: MediaQueryList
+}
+
+function themeSwitch({ html, mq }: ThemeSwitchType) {
+  const theme = mq.matches ? "dark" : "light"
+  html.setAttribute("data-theme", theme)
+}
+
+export function themeHandler() {
+  const html = document.documentElement
+  const mq = window.matchMedia('(prefers-color-scheme: dark)')
+
+  mq.addEventListener("change", () => {
+    themeSwitch({ html, mq })
+  })
+
+  themeSwitch({ html, mq })
+}
