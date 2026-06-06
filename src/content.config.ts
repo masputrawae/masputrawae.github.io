d4792d7e38292e0bab3dc4069f9ac2014c1b4e11
masputrawae <masputrawae@users.noreply.github.com>
2026-06-06 09:20:25 +0700

    feat: add content collection

---
+++ src/content.config.ts
@@ -0,0 +1,51 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+import path from "path"
+
+const contentDir = path.resolve("registry/content")
+const pageType = z.enum([
+  "BlogPosting",
+  "Article",
+  "NewsArticle",
+  "Person",
+  "AboutPage",
+  "WebPage",
+  "WebSite",
+  "CollectionPage"
+])
+
+const schema = z.object({
+  title: z.string(),
+  description: z.string(),
+
+  pubDate: z.coerce.date(),
+  updatedDate: z.coerce.date().optional(),
+
+  tags: z.array(z.string()),
+  categories: z.array(z.string()),
+
+  pinned: z.boolean().default(false),
+  draft: z.boolean().default(false),
+  pageType: pageType
+})
+
+
+export const collections = {
+  // Section
+  section: defineCollection({
+    loader: glob({ base: contentDir, pattern: "**/*_index.md" }),
+    schema: schema.extend({
+      pageType: pageType.default("CollectionPage")
+    })
+  }),
+
+  // Page
+  page: defineCollection({
+    loader: glob({ base: contentDir, pattern: ["**/*.md", "!**/*_index.md"] }),
+    schema: schema.extend({
+      pageType: pageType.default("BlogPosting")
+    })
+  })
+}
+
